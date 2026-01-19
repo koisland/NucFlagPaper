b4b41fa57ca34a3329f9f13f252796712a8ad665
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2026-01-19 17:28:35 -0500

    Rewrite cmp_all_calls in assembly wf to not use catplot.

--- workflow/rules/compare_assembly/compare_all.smk
+++ workflow/rules/compare_assembly/compare_all.smk
@@ -5,17 +5,41 @@ rule plot_compare_misassemblies:
         calls=[
             expand(
                 rules.nf_denovo_check_asm_nucflag.output.misassemblies,
-                sm="{sm}_hifi",
+                sm=f"{sm}_hifi",
             )
             for sm in config["samples"].keys()
         ],
     output:
-        bed=join(OUTPUT_DIR, "nucflag", "all_calls_compared.png"),
+        plot=join(OUTPUT_DIR, "nucflag", "all_calls_compared.png"),
     params:
         script="workflow/scripts/compare_assembly/cmp_all_calls.py",
         labels=" ".join(config["samples"].keys()),
         colors=" ".join(ASM_COLORS.get(sm) for sm in config["samples"].keys()),
     shell:
         """
-        python {params.script} -i {input.calls} -l {params.labels} -c {params.colors}
+        python {params.script} -i {input.calls} -l {params.labels} -c {params.colors} -o {output.plot}
         """
+
+
+rule overlap_annotation_data:
+    input:
+        censat=rules.liftover_annotations.output.censat,
+        calls=expand(
+            rules.nf_denovo_check_asm_nucflag.output.misassemblies,
+            sm="{sm}_hifi",
+        ),
+    output:
+        censat=join(OUTPUT_DIR, "sequence_ctx", "{sm}_censat.bed"),
+    shell:
+        """
+        bedtools intersect -loj -a <(cut -f1-4 {input.calls}) -b <(awk -v OFS="\\t" '{{ print $1, $2, $3, $4, $9 }}' {input.censat}) | \
+            awk -v OFS="\\t" '{{ if ($5 == ".") {{ $8="no_overlap"; $9="128,128,128"; }}; print}}' > {output.censat}
+        """
+
+
+# TODO: Also generate context?
+
+
+rule compare_all:
+    input:
+        rules.plot_compare_misassemblies.output,
--- workflow/scripts/compare_assembly/cmp_all_calls.py
+++ workflow/scripts/compare_assembly/cmp_all_calls.py
@@ -1,8 +1,12 @@
 import argparse
+from matplotlib.colors import rgb2hex
 import polars as pl
 import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as pe
 
 from matplotlib.axes import Axes
+from matplotlib.text import Text
 
 
 # https://stackoverflow.com/a/67594395
@@ -10,34 +14,68 @@ def update_bars(ax: Axes, round_to: int):
     for c in ax.containers:
         labels = [f"{round(v.get_height() / 1_000_000, round_to)}" for v in c]
         ax.bar_label(c, labels=labels, label_type="edge")
-
-    ylim = ax.get_ylim()
-    yticks = range(int(ylim[0]), int(ylim[1]), 10_000_000)
-    ax.set_yticks(yticks, [f"{round(tick / 1_000_000, round_to)}" for tick in yticks])
     ax.set_xlabel(None)
 
 
-def draw_bar_all(df_all: pl.DataFrame, colors: dict[str, str], outfile: str):
-    g = sns.catplot(
-        data=df_all,
-        x="name",
-        y="length",
-        col="lbl",
-        hue="lbl",
-        sharex=False,
-        kind="bar",
-        legend=None,
-        aspect=1.5,
-        palette=colors,
+def rgb_to_hex(srs: pl.Series) -> pl.Series:
+    color_hex = []
+    for elem in srs:
+        if elem.startswith("#"):
+            color_hex.append(elem)
+        else:
+            rgb = tuple(int(e) / 255 for e in elem.split(","))
+            assert len(rgb) == 3, f"Invalid item_rgb format for {rgb}"
+            color_hex.append(rgb2hex(rgb))
+    return pl.Series(name="color", values=color_hex)
+
+
+def draw_bar_all(
+    df_all: pl.DataFrame,
+    colors: dict[str, str],
+    name_colors: dict[str, str],
+    col_order: list[str],
+    outfile: str,
+):
+    fig, axes = plt.subplots(
+        nrows=1,
+        ncols=len(col_order),
+        figsize=(16, 4),
+        layout="constrained",
+        sharex=True,
+        sharey=True,
     )
-    for ax in g.axes.ravel():
+    max_length = df_all["length"].max()
+    yticks = list(range(0, int(max_length), 1_000_000))
+    yticklabels = [f"{tick / 1_000_000}" for tick in yticks]
+    for i, col in enumerate(col_order):
+        ax: Axes = axes[i]
+        df_col = df_all.filter(pl.col("lbl") == col)
+
+        sns.barplot(
+            data=df_col,
+            x="name",
+            y="length",
+            hue="lbl",
+            legend=None,
+            palette=colors,
+            ax=ax,
+        )
+
         update_bars(ax, round_to=2)
+        for lbl in ax.get_xticklabels():
+            lbl: Text
+            # Color and stroke around text
+            lbl.set_color(name_colors[lbl.get_text()])
+            lbl.set_path_effects([pe.Stroke(linewidth=0.2, foreground="black")])
 
-    g.set_titles(row_template="{row_name}", col_template="{col_name}")
+        ax.set_yticks(yticks, yticklabels)
+        ax.set_title(col)
+        ax.tick_params(axis="x", rotation=45)
+        for spine in ("top", "right"):
+            ax.spines[spine].set_visible(False)
 
-    g.tick_params(axis="x", rotation=45)
-    g.set_ylabels("Length (Mbp)")
-    g.savefig(outfile, dpi=600, bbox_inches="tight")
+    fig.supylabel("Length (Mbp)")
+    fig.savefig(outfile, dpi=600, bbox_inches="tight")
 
 
 def main():
@@ -51,21 +89,25 @@ def main():
 
     colors = dict(zip(args.labels, args.colors, strict=True))
 
-    df_all = (
-        pl.concat(
-            [
-                pl.read_csv(call, separator="\t", has_header=True).with_columns(
-                    lbl=pl.lit(label)
-                )
-                for call, label in zip(args.calls, args.labels, strict=True)
-            ]
-        )
-        .with_columns(length=pl.col("chromEnd") - pl.col("chromStart"))
-        .group_by(["lbl", "name"])
-        .agg(pl.col("length").sum())
+    df_all = pl.concat(
+        [
+            pl.read_csv(call, separator="\t", has_header=True)
+            .with_columns(lbl=pl.lit(label))
+            .filter(~pl.col("name").eq("correct"))
+            for call, label in zip(args.input_calls, args.labels, strict=True)
+        ]
+    ).with_columns(
+        length=pl.col("chromEnd") - pl.col("chromStart"),
+        itemRgb=pl.col("itemRgb").map_batches(rgb_to_hex, return_dtype=pl.String),
     )
-
-    draw_bar_all(df_all, colors=colors, outfile=args.outfile)
+    name_colors = dict(df_all.select("name", "itemRgb").iter_rows())
+    draw_bar_all(
+        df_all.group_by(["lbl", "name"]).agg(pl.col("length").sum()),
+        colors=colors,
+        col_order=args.labels,
+        name_colors=name_colors,
+        outfile=args.outfile,
+    )
 
 
 if __name__ == "__main__":
